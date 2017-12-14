e76f834b9f55a50d8e88f51a0f495609d57a1f9e
Waseem <Waseem@MYPC>
2017-12-14 09:27:30 -0800

    Change

--- AngAsp/Controllers/FModelsController.cs
+++ AngAsp/Controllers/FModelsController.cs
@@ -18,7 +18,7 @@ namespace AngAsp.Controllers
         // GET: FModels
         public async Task<ActionResult> Index()
         {
-            ViewBag.Name = "Waseem";
+            ViewBag.Name = "Waseem Feroz";
             return View(await db.MyModel.ToListAsync());
         }
 
